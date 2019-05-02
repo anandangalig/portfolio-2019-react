c6f651c15c9bef3e5cdbc7349b18455332bfe751
Anand Angalig <anandangalig@gmail.com>
2019-05-02 19:47:20 -0400

    update AboutMe style to be mobile friendly

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -12,10 +12,12 @@ const AboutMe = props => {
 
       <div className="about about_blurb--text">
         <span>Hey, I’m Anand.</span>
-        <p>I’m a Philly based web developer currently specializing in JavaScript and React.</p>
         <p>
-          I believe in lifelong learning, both in my personal and professional life. Recently
-          decided to learn how to play guitar. It’s not going so well...
+          I’m a Philadelphia based web developer currently specializing in JavaScript and React.
+        </p>
+        <p>
+          I am curios, and always open to trying new things. I believe in lifelong learning, both in
+          my personal and professional life.
         </p>
         <p>
           At work, I bring professionalism and positive attitude. At home, I make chicken
@@ -37,15 +39,16 @@ const AboutMe = props => {
         </ul>
       </div>
       <div className="about about_misc">
-        <span>Misc.</span>
+        <span>Fun Facts</span>
         <ul>
           <li>Was born in Mongolia</li>
           <li>76'ers fan</li>
           <li>Can train you in Muay Thai</li>
-          <li>Partied with "Big Lebowski" one time</li>
-          <li>Pro sushi chef</li>
-          <li>Terrible singer</li>
-          <li>Proficient in Russian</li>
+          <li>Partied with the Dude "Big Lebowski" one time</li>
+          <li>Sushi chef at heart</li>
+          <li>Loves the NPR</li>
+          <li>Speaks Russian</li>
+          <li>Favorite TV show: What is Jeopardy?</li>
         </ul>
       </div>
     </div>
