b98f8261c39f60ec65cb5e63f4225094b0a6b7c8
Anand Angalig <anandangalig@gmail.com>
2019-05-03 16:45:15 -0400

    Projects component mobile style is almost done

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -4,40 +4,40 @@ import './ProjectCard.scss';
 const ProjectCard = ({ project }) => {
   let { title, description, image, github_link, link, link_external, tech_used } = project;
   return (
-    <div>
-      <div className="project__card">
-        <div className="project__title">{title}</div>
-        <div className="project__desc">{description}</div>
-        <div className="project__image">
-          <img alt={title} src={`/images/${image}`} />
-        </div>
-        <div className="project__tech-used">
-          <ul>
-            {tech_used.map(item => {
-              return <li key={item}>{item}</li>;
-            })}
-          </ul>
-        </div>
-        <div className="project__links">
-          <a
-            className="project__link project__link--github"
-            href={github_link}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>
-          <a
-            className="project__link project__link--hosted"
-            href={link}
-            target={link_external ? '_blank' : '_self'}
-            rel="noopener noreferrer"
-          >
-            View Project
-          </a>
-        </div>
+    <div className="project">
+      <div className="project__title">{title}</div>
+      <div className="project__desc">{description}</div>
+      <div className="project__image">
+        <img alt={title} src={`/images/${image}`} />
       </div>
-      <hr />
+      <div className="project__tech-used">
+        <ul>
+          {tech_used.map(item => {
+            return <li key={item}>{item}</li>;
+          })}
+        </ul>
+      </div>
+      <div className="project__links">
+        <a
+          className="project__link project__link--github"
+          href={github_link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        <a
+          className="project__link project__link--hosted"
+          href={link}
+          target={link_external ? '_blank' : '_self'}
+          rel="noopener noreferrer"
+        >
+          View Project
+        </a>
+      </div>
+      <div className="hr">
+        <hr />
+      </div>
     </div>
   );
 };
